2b02d20bd9ac8d246eed82325b98183c737a3cd5
Justyna Szewczyk <justyna0829@gmail.com>
2021-08-06 22:25:51 +0200

    Add department.model tests

---
+++ models/test/department.test.js
@@ -0,0 +1,49 @@
+const Department = require('../department.model');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Department', () => {
+  it('should throw an error if no "name" arg', () => {
+    const dep = new Department({}); // create new Department, bit dont't set `name` attr value
+    dep.validate(err => {
+      expect(err.errors.name).to.exist;
+    })
+  });
+
+  it('should throw an error if "name" is not a string', () => {
+    const cases = [{}, []];
+    for(let name of cases) {
+      const dep = new Department({ name });
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should throw an error if "name" is <5 or >20', () => {
+    const cases = ['Abc', 'abcd', 'Lorem Ipsum, Lorem Ip'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+      dep.validate(err => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+
+  it('should not throw an error if "name" is okay', () => {
+    const cases = ['Management', 'Human Resources'];
+    for(let name of cases) {
+      const dep = new Department({ name });
+      dep.validate(err => {
+        expect(err).to.not.exist;
+      });
+    }
+  });  
+  
+  after(() => {
+    mongoose.models = {};
+  });
+  
+});
+
+
